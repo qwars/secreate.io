86ccbc5061605d61304d18282ec3c2c5b223cbf1
Александр Селюнин <aka.qwars@gmail.com>
2020-04-17 21:11:49 +0500

    Merge client

---
+++ public/collection.js
@@ -0,0 +1,14 @@
+
+function Collection(dbi){
+	this._dbi = dbi;
+};
+exports.Collection = Collection; // export class 
+Collection.prototype.getContent = function (){
+	return new Promise(function(resolve,reject) {  });
+};
+
+Collection.prototype.setContent = function (){
+	return new Promise(function(resolve,reject) {  });
+};
+
+
