d3b9506e4c3d3735516e9272d3a5dc5fe9b87f36
Александр Селюнин <aka.qwars@gmail.com>
2020-04-17 21:25:47 +0500

    Merge server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
+var self = {};
 var DataBase = require('./database').DataBase;
+var Collection = require('./collection').Collection;
 var WebSocketServer = require('websocket').server;
 
 const app = require('express')();
@@ -8,6 +10,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 
 const db = new DataBase('mongodb://mongo:27017/');
+const collection = new Collection(db);
 
 app.use(bodyParser.urlencoded(
 	{extended: false}
@@ -15,10 +18,10 @@ app.use(bodyParser.urlencoded(
 app.use(bodyParser.json());
 
 const clientUrl = 'http://localhost:8081';
-const corsOptions = {
+const corsOptions = cors({
 	origin: clientUrl,
 	optionsSuccessStatus: 200
-};
+});
 
 app.use(cors);
 app.all('/*',function(req,res,next) {
@@ -27,6 +30,7 @@ app.all('/*',function(req,res,next) {
 	return next();
 });
 
+app.listen(3000);
 
 const connections = new Set();
 const lastModification = 0;
@@ -39,12 +43,30 @@ const socket = new WebSocketServer(
 
 server.listen(8999,function() { return console.log(("Server started on port " + (server.address().port) + " :)")); });
 
+const observerConnections = function(resource) { return connections.forEach(function(connection) { return connection.send(resource); }); };
 
 socket.on('connection',function(request) {
 	
 	const connection = request;
 	connections.add(connection);
-	// запросы к базе по текущему состоянию
+	collection.getContent().then(
+		function(resource) { return connection.send(self.res().json(resource)); },
+		function(err) { return self.res().json('error',err); }
+	);
 	
-	return socket.on('close',function(request) {  }); // connections.delete connection
+	return socket.on('close',function(request) { return connections.delete(connection); });
+});
+
+app.get('/elements',corsOptions,function(req,res) {
+	return collection.getContent().then(
+		function(resource) { return res.json(resource); },
+		function(err) { return res.json('error',err); }
+	);
+});
+
+app.post('/elements',corsOptions,function(req,res) {
+	return collection.setContent(req.body().order()).then(
+		function(resource) { return observerConnections(JSON.stringify(resource)); },
+		function(err) { return res.json('error',err); }
+	);
 });
