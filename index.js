136f2494fe273dada05cc9f3cb604d226a0aa0be
Александр Селюнин <aka.qwars@gmail.com>
2020-04-18 10:07:00 +0500

    Generator content data

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const request = require('request');
+
+const distance = Number( process.argv[ process.argv.length - 1 ] ) ||  1000
+
+const startGenerator = () => request({
+    url: "http://localhost:3000",
+    method: "POST",
+    json: true,
+    body: Array( 20 ).fill(0).map( () => Array( 10 ).fill(0).map( () => Math.random() ).map( (n) => Math.random() * ( Number( n.toFixed( 1 ) ) === 0.5 ? 0 : ( n > 0.5 ? 1 : -1 ) ) ) )
+}, (error, resp, body) => console.log(error, resp, body) )
+
+
+
+setInterval( startGenerator, distance )
